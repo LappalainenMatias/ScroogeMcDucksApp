0bf4acb130c8595142acef45fa7455cddb6452cb
Lappalainen_PSPR <Matias.Lappalainen1@gmail.com>
2021-02-21 15:21:14 +0200

    Update StockFragment.java

--- app/src/main/java/com/example/stockanalyzer/ui/StockFragment.java
+++ app/src/main/java/com/example/stockanalyzer/ui/StockFragment.java
@@ -1,4 +1,30 @@
 package com.example.stockanalyzer.ui;
 
-public class StockFragment {
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.example.stockanalyzer.R;
+import com.example.stockanalyzer.viewmodels.StockListViewModel;
+
+public class StockFragment extends Fragment {
+
+    private StockListViewModel mViewModel;
+
+    public static StockListFragment newInstance() {
+        return new StockListFragment();
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.stock_fragment, container, false);
+    }
+
 }
