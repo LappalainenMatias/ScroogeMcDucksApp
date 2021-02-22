317233ff02f8f420d4f53c9dbf52e4962b4786dc
Lappalainen_PSPR <Matias.Lappalainen1@gmail.com>
2021-02-22 16:05:32 +0200

    Add example spinner

--- app/src/main/java/com/example/stockanalyzer/ui/StockFragment.java
+++ app/src/main/java/com/example/stockanalyzer/ui/StockFragment.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 import android.widget.Toolbar;
 
 import androidx.annotation.NonNull;
@@ -17,8 +19,8 @@ public class StockFragment extends Fragment {
 
     private StockListViewModel mViewModel;
 
-    public static StockListFragment newInstance() {
-        return new StockListFragment();
+    public static StockFragment newInstance() {
+        return new StockFragment();
     }
 
     View containerView;
@@ -27,8 +29,20 @@ public class StockFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        containerView =  inflater.inflate(R.layout.stock_fragment, container, false);
+        containerView = inflater.inflate(R.layout.stock_fragment, container, false);
+        Spinner spinner = containerView.findViewById(R.id.SpinnerCategories);
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(getActivity(), android.R.layout.simple_spinner_dropdown_item, getCategories());
+        adapter.setDropDownViewResource(R.layout.item_spinner);
+        spinner.setAdapter(adapter);
         return containerView;
+    }
+
+    public String[] getCategories(){
+        String[] categories = new String[3];
+        categories[0] = getResources().getString(R.string.categories_1);
+        categories[1] = getResources().getString(R.string.categories_2);
+        categories[2] = getResources().getString(R.string.categories_3);
+        return categories;
     }
 
 }
