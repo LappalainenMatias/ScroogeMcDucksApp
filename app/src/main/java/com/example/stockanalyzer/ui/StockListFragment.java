a6f9269f7debebcd9847dc861299de76edbe88ef
Lappalainen_PSPR <Matias.Lappalainen1@gmail.com>
2021-02-22 13:41:36 +0200

    Create example listview for testing

--- app/src/main/java/com/example/stockanalyzer/ui/StockListFragment.java
+++ app/src/main/java/com/example/stockanalyzer/ui/StockListFragment.java
@@ -1,5 +1,6 @@
 package com.example.stockanalyzer.ui;
 
+import androidx.appcompat.widget.Toolbar;
 import androidx.lifecycle.ViewModelProviders;
 
 import android.os.Bundle;
@@ -11,30 +12,61 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Adapter;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import com.example.stockanalyzer.R;
 import com.example.stockanalyzer.viewmodels.StockListViewModel;
+
+import java.util.ArrayList;
 
 public class StockListFragment extends Fragment {
 
     private StockListViewModel mViewModel;
 
-    public static StockListFragment newInstance() {
-        return new StockListFragment();
-    }
+    View viewContainer;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.stocklist_fragment, container, false);
+        viewContainer = inflater.inflate(R.layout.stocklist_fragment, container, false);
+        ListView listView = viewContainer.findViewById(R.id.LVStocks);
+        ArrayList<String> items = new ArrayList<>();
+        items.add("1");
+        items.add("2");
+        items.add("1");
+        items.add("2");
+        items.add("1");
+        items.add("2");
+        items.add("1");
+        items.add("2");
+        items.add("1");
+        items.add("2");
+        items.add("1");
+        items.add("2");
+        items.add("1");
+        items.add("2");
+        items.add("1");
+        items.add("2");
+        items.add("1");
+        items.add("2");
+        items.add("1");
+        items.add("2");
+        items.add("1");
+        items.add("2");
+        items.add("1");
+        items.add("2");
+        ArrayAdapter<String> adapt = new ArrayAdapter<String>(getContext(), android.R.layout.simple_list_item_1, items);
+        listView.setAdapter(adapt);
+        return viewContainer;
     }
 
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
         mViewModel = ViewModelProviders.of(this).get(StockListViewModel.class);
-        // TODO: Use the ViewModel
     }
 
 }
