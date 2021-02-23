e211bacdd4bcb38d206218fc310f066dc89f86d3
LappalainenMatias <46249975+LappalainenMatias@users.noreply.github.com>
2021-02-23 21:29:39 +0200

    Create repository for example data

--- app/src/main/java/com/example/stockanalyzer/repository/Repository.java
+++ app/src/main/java/com/example/stockanalyzer/repository/Repository.java
@@ -1,4 +1,26 @@
 package com.example.stockanalyzer.repository;
 
+import com.example.stockanalyzer.exampledata.ExampleData;
+import com.example.stockanalyzer.stock.StockItem;
+
+import java.util.ArrayList;
+
 public class Repository {
+    public ArrayList<StockItem> getStocks(){
+        ArrayList<StockItem> stockItems = new ArrayList<>();
+        ExampleData exampleData = new ExampleData();
+        stockItems.add(exampleData.exampleStockItem1());
+        stockItems.add(exampleData.exampleStockItem2());
+        return stockItems;
+    }
+
+    public StockItem getStock(int id){
+        ArrayList<StockItem> stockItems = getStocks();
+        for(StockItem stockItem : stockItems){
+            if(stockItem.id == id){
+                return stockItem;
+            }
+        }
+        return null;
+    }
 }
