8428dfddd94f876eb2dd65d45756a6a5b9c2d909
LappalainenMatias <46249975+LappalainenMatias@users.noreply.github.com>
2021-02-23 21:32:00 +0200

    Create stockItems MutableLiveData

--- app/src/main/java/com/example/stockanalyzer/viewmodels/StockListViewModel.java
+++ app/src/main/java/com/example/stockanalyzer/viewmodels/StockListViewModel.java
@@ -1,6 +1,27 @@
 package com.example.stockanalyzer.viewmodels;
 
+import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
+import com.example.stockanalyzer.repository.Repository;
+import com.example.stockanalyzer.stock.OpeningPriceSMA5;
+import com.example.stockanalyzer.stock.StockItem;
+
+import java.util.List;
+
 public class StockListViewModel extends ViewModel {
+    MutableLiveData<List<StockItem>> stockItems;
+
+    public MutableLiveData<List<StockItem>> getStockItems() {
+        if (stockItems == null) {
+            stockItems = new MutableLiveData<List<StockItem>>();
+            loadStockItems();
+        }
+        return stockItems;
+    }
+
+    private void loadStockItems() {
+        Repository repository = new Repository();
+        stockItems.setValue(repository.getStocks());
+    }
 }
