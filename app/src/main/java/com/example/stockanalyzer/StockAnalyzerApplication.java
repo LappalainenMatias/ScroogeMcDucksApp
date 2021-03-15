4d6a909df1ee7114fadf999baca35d0b0c331ad1
LappalainenMatias <46249975+LappalainenMatias@users.noreply.github.com>
2021-03-15 15:36:10 +0200

    Rename MyApplication to StockAnalyzerApplication

---
+++ app/src/main/java/com/example/stockanalyzer/StockAnalyzerApplication.java
@@ -0,0 +1,9 @@
+package com.example.stockanalyzer;
+
+import android.app.Application;
+
+import dagger.hilt.android.HiltAndroidApp;
+
+@HiltAndroidApp
+public class StockAnalyzerApplication extends Application {
+}
