54506bebb7cf6c95fbb04b7971a0e552557a5e9f
LappalainenMatias <46249975+LappalainenMatias@users.noreply.github.com>
2021-03-15 15:45:33 +0200

    Add selectedFileUri and method for saving files

--- app/src/main/java/com/example/stockanalyzer/viewmodels/StockListViewModel.java
+++ app/src/main/java/com/example/stockanalyzer/viewmodels/StockListViewModel.java
@@ -1,16 +1,29 @@
 package com.example.stockanalyzer.viewmodels;
 
+import android.net.Uri;
+
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
-import com.example.stockanalyzer.repository.Repository;
-import com.example.stockanalyzer.stock.OpeningPriceSMA5;
+import com.example.stockanalyzer.repository.StockRepository;
 import com.example.stockanalyzer.stock.StockItem;
 
 import java.util.List;
+import javax.inject.Inject;
+import dagger.hilt.android.lifecycle.HiltViewModel;
 
+@HiltViewModel
 public class StockListViewModel extends ViewModel {
+
     MutableLiveData<List<StockItem>> stockItems;
+    MutableLiveData<Uri> selectedFileUri;
+
+    StockRepository stockRepository;
+
+    @Inject
+    public StockListViewModel(StockRepository stockRepository){
+        this.stockRepository = stockRepository;
+    }
 
     public MutableLiveData<List<StockItem>> getStockItems() {
         if (stockItems == null) {
@@ -21,7 +34,28 @@ public class StockListViewModel extends ViewModel {
     }
 
     private void loadStockItems() {
-        Repository repository = new Repository();
-        stockItems.setValue(repository.getStocks());
+        stockItems.setValue(stockRepository.getStockItems());
+    }
+
+    /**
+     * Uri for the file user is saving to the internal storage.
+     * Null if no file has been selected
+     */
+    public MutableLiveData<Uri> getSelectedFileUri() {
+        if(selectedFileUri == null){
+            selectedFileUri = new MutableLiveData<>(null);
+        }
+        return selectedFileUri;
+    }
+
+    /**
+     * @param fileName this is also the name for the stock which is shown to the user
+     * @return true if file was saved successfully
+     */
+    public boolean saveStockDataFile(String fileName) {
+        if(getSelectedFileUri() == null || getSelectedFileUri().getValue() == null){
+            return false;
+        }
+        return stockRepository.saveFile(getSelectedFileUri().getValue(), fileName);
     }
 }
