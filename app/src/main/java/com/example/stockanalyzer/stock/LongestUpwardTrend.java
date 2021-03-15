8e1dc330a978bf517ee4f34f9da1243e710c0137
LappalainenMatias <46249975+LappalainenMatias@users.noreply.github.com>
2021-03-15 15:39:06 +0200

    Add comment

--- app/src/main/java/com/example/stockanalyzer/stock/LongestUpwardTrend.java
+++ app/src/main/java/com/example/stockanalyzer/stock/LongestUpwardTrend.java
@@ -3,12 +3,21 @@ package com.example.stockanalyzer.stock;
 import java.util.GregorianCalendar;
 
 public class LongestUpwardTrend {
-    public int size;
+
+    public int length;
     public GregorianCalendar start;
     public GregorianCalendar end;
 
-    public LongestUpwardTrend(int size, GregorianCalendar start, GregorianCalendar end) {
-        this.size = size;
+    /**
+     * Upward trend means closing price of day N is higher than closing price of day N-1
+     * @param length Amount of days the upward trend has lasted.
+     *               This is NOT always equal to the days between start and end because
+     *               stock is not active every day for example in the weekends.
+     * @param start Start of the upward trend
+     * @param end End of the upward trend
+     */
+    public LongestUpwardTrend(int length, GregorianCalendar start, GregorianCalendar end) {
+        this.length = length;
         this.start = start;
         this.end = end;
     }
