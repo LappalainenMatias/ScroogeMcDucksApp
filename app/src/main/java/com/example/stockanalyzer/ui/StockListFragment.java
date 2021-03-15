f420dcd83cbb93ba08b5796b92a5a47acc3bf4fa
LappalainenMatias <46249975+LappalainenMatias@users.noreply.github.com>
2021-03-15 15:44:48 +0200

    Refactor
    Added @AndroidEntryPoint, removed onAttach, created selectedFileUri observer

--- app/src/main/java/com/example/stockanalyzer/ui/StockListFragment.java
+++ app/src/main/java/com/example/stockanalyzer/ui/StockListFragment.java
@@ -1,37 +1,30 @@
 package com.example.stockanalyzer.ui;
 
-import androidx.activity.result.ActivityResult;
-import androidx.activity.result.ActivityResultCallback;
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AlertDialog;
-import androidx.appcompat.widget.ButtonBarLayout;
 import androidx.appcompat.widget.SearchView;
 import androidx.core.content.ContextCompat;
-import androidx.lifecycle.ViewModelProviders;
+import androidx.lifecycle.ViewModelProvider;
 
 import android.app.Activity;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Color;
-import android.graphics.drawable.Drawable;
 import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.navigation.NavController;
-import androidx.navigation.NavDirections;
 import androidx.navigation.Navigation;
-import androidx.navigation.fragment.NavHostFragment;
 
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
@@ -41,34 +34,44 @@ import android.widget.Toast;
 
 import com.example.stockanalyzer.R;
 import com.example.stockanalyzer.arrayadapter.StockArrayAdapter;
-import com.example.stockanalyzer.stock.StockItem;
 import com.example.stockanalyzer.viewmodels.StockListViewModel;
 import com.google.android.material.button.MaterialButton;
 import com.google.android.material.dialog.MaterialAlertDialogBuilder;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.textfield.TextInputLayout;
 
-import java.util.ArrayList;
+import dagger.hilt.android.AndroidEntryPoint;
 
+
+@AndroidEntryPoint
 public class StockListFragment extends Fragment {
 
-    private StockListViewModel stockListViewModel;
-
     View viewContainer;
-    private MaterialButton materialButton;
+    private Button BTNAddFile;
+    private MaterialButton BTNChooseFile;
+    private TextInputLayout inputStockName;
     private ListView LVStocks;
-    private StockArrayAdapter stockAdapter;
     private MaterialAlertDialogBuilder materialAlertDialogBuilder;
+    private AlertDialog AlertDialogStockData;
+
+    private StockListViewModel stockListViewModel;
+    private StockArrayAdapter stockAdapter;
+    private ActivityResultLauncher<Intent> resultLauncher;
+    private Intent intent;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
         viewContainer = inflater.inflate(R.layout.stocklist_fragment, container, false);
+
+        // TODO: 10.3.2021 Refactor. Rename views
         SearchView searchView = viewContainer.findViewById(R.id.action_search);
+        LVStocks = viewContainer.findViewById(R.id.LVStocks);
+        FloatingActionButton FABAdd = viewContainer.findViewById(R.id.FABAdd);
         TextView textView = searchView.findViewById(androidx.appcompat.R.id.search_src_text);
         ImageView searchIcon = searchView.findViewById(androidx.appcompat.R.id.search_button);
         ImageView searchClose = searchView.findViewById(androidx.appcompat.R.id.search_close_btn);
-        FloatingActionButton FABAdd = viewContainer.findViewById(R.id.FABAdd);
 
         searchIcon.setImageDrawable(ContextCompat.getDrawable(getActivity(), R.drawable.ic_search_24px));
         textView.setTextColor(Color.WHITE);
@@ -77,18 +80,20 @@ public class StockListFragment extends Fragment {
 
         FABAdd.setOnClickListener(v -> {
             materialAlertDialogBuilder = new MaterialAlertDialogBuilder(getContext())
-                    .setTitle("Add stock data")
-                    .setMessage("Application only supports csv files.")
-                    .setView(getMaterialButton())
+                    .setTitle("Choose a file (.csv) which contains stock data")
+                    .setView(getAttachFileLayout())
                     .setPositiveButton("ADD", (dialogInterface, i) -> {
+                        if (stockListViewModel.saveStockDataFile(inputStockName.getEditText().getText().toString())) {
+                            Toast.makeText(getContext(), "Successfully saved stock data!", Toast.LENGTH_LONG).show();
+                        } else {
+                            Toast.makeText(getContext(), "Failed to save stock data!", Toast.LENGTH_LONG).show();
+                        }
                     })
                     .setNegativeButton("CANCEL", (dialogInterface, i) -> {
                     });
-
-            AlertDialog alertDialog = materialAlertDialogBuilder.show();
-            Button button = alertDialog.getButton(DialogInterface.BUTTON_POSITIVE);
-            button.setAlpha(0.5f);
-            button.setClickable(false);
+            AlertDialogStockData = materialAlertDialogBuilder.show();
+            BTNAddFile = AlertDialogStockData.getButton(DialogInterface.BUTTON_POSITIVE);
+            stockListViewModel.getSelectedFileUri().setValue(null);
         });
 
         searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
@@ -99,62 +104,93 @@ public class StockListFragment extends Fragment {
 
             @Override
             public boolean onQueryTextChange(String newText) {
-                if (stockAdapter.getFilter() != null) {
-                    stockAdapter.getFilter().filter(newText);
-                }
+                stockAdapter.getFilter().filter(newText);
                 return false;
             }
         });
         return viewContainer;
     }
 
-    private ActivityResultLauncher<Intent> resultLauncher;
-    private Intent intent;
+    // TODO: 13.3.2021 Rotation cases exception
 
     @Override
-    public void onAttach(@NonNull Context context) {
-        super.onAttach(context);
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        stockListViewModel = new ViewModelProvider(this).get(StockListViewModel.class);
         intent = new Intent(Intent.ACTION_GET_CONTENT);
-        Uri uri = Uri.parse("/downloads"); // a directory
-        intent.setDataAndType(uri, "*/*");
+        intent.setDataAndType(Uri.parse("/downloads"), "text/csv");
         resultLauncher = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(),
                 result -> {
                     if (result.getResultCode() == Activity.RESULT_OK) {
+                        Toast.makeText(getContext(), "File selected!", Toast.LENGTH_SHORT).show();
                         Intent data = result.getData();
-                        materialButton.setText(data.getPackage());
+                        Uri uri = data.getData();
+                        stockListViewModel.getSelectedFileUri().setValue(uri);
+                    } else {
+                        Toast.makeText(getContext(), "Something went wrong!", Toast.LENGTH_SHORT).show();
                     }
                 });
-    }
-
-    private LinearLayout getMaterialButton() {
-        LayoutInflater inflater = (LayoutInflater) getActivity().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-        LinearLayout linearLayout = (LinearLayout) inflater.inflate(R.layout.choose_file_btn, null, true);
-        materialButton = linearLayout.findViewById(R.id.BTNChooseFile);
-        materialButton.setOnClickListener(v -> {
-            resultLauncher.launch(intent);
-        });
-
-        return linearLayout;
-    }
-
-
-    // TODO: 25/02/2021 Replace deprecated code
-    @Override
-    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
-        super.onActivityCreated(savedInstanceState);
-        stockListViewModel = ViewModelProviders.of(this).get(StockListViewModel.class);
-        LVStocks = viewContainer.findViewById(R.id.LVStocks);
-
         stockListViewModel.getStockItems().observe(getViewLifecycleOwner(), stockItems -> {
             stockAdapter = new StockArrayAdapter(getContext(), getActivity(), stockItems);
             LVStocks.setAdapter(stockAdapter);
         });
 
-        LVStocks.setOnItemClickListener((parent, view, position, id) -> {
+        stockListViewModel.getSelectedFileUri().observe(getViewLifecycleOwner(), selectedFileUri -> {
+            // TODO: 12.3.2021 Refactor
+            if (BTNChooseFile == null || inputStockName == null || BTNAddFile == null) {
+                return;
+            }
+            if (selectedFileUri == null) {
+                BTNChooseFile.setVisibility(View.VISIBLE);
+                inputStockName.setVisibility(View.GONE);
+                BTNAddFile.setAlpha(0.5f);
+                BTNAddFile.setClickable(false);
+            } else {
+                AlertDialogStockData.setTitle("Add a name for the stock data");
+                BTNChooseFile.setVisibility(View.GONE);
+                inputStockName.setVisibility(View.VISIBLE);
+                inputStockName.requestFocus();
+
+                inputStockName.getEditText().addTextChangedListener(new TextWatcher() {
+                    @Override
+                    public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+                    }
+
+                    @Override
+                    public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+                    }
+
+                    @Override
+                    public void afterTextChanged(Editable editable) {
+                        if (editable.length() > 0) {
+                            BTNAddFile.setAlpha(1f);
+                            BTNAddFile.setClickable(true);
+                        } else {
+                            BTNAddFile.setAlpha(0.5f);
+                            BTNAddFile.setClickable(false);
+                        }
+                    }
+                });
+            }
+        });
+
+        LVStocks.setOnItemClickListener((parent, v, position, id) -> {
             Bundle bundle = new Bundle();
-            bundle.putInt("stockId", stockAdapter.stockItems.get(position).id);
-            Navigation.findNavController(view).navigate(R.id.action_stockListFragment_to_stockFragment, bundle);
+            bundle.putString("stockFileName", stockAdapter.stockItems.get(position).fileName);
+            Navigation.findNavController(v).navigate(R.id.action_stockListFragment_to_stockFragment, bundle);
         });
     }
 
+    private LinearLayout getAttachFileLayout() {
+        LayoutInflater inflater = (LayoutInflater) getActivity().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        LinearLayout linearLayout = (LinearLayout) inflater.inflate(R.layout.choose_file_btn, null, true);
+        BTNChooseFile = linearLayout.findViewById(R.id.BTNChooseFile);
+        inputStockName = linearLayout.findViewById(R.id.LayoutStocksName);
+        BTNChooseFile.setOnClickListener(v -> {
+            resultLauncher.launch(intent);
+        });
+        return linearLayout;
+    }
 }
