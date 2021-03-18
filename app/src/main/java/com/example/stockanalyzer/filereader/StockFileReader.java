b34c90fbfb36a10a1c2b1938e7eb686dabbdfa18
Matias Lappalainen <Matias.lappalainen1@gmail.com>
2021-03-19 00:10:20 +0200

    Add comment and add support for multiple file paths

--- app/src/main/java/com/example/stockanalyzer/filereader/StockFileReader.java
+++ app/src/main/java/com/example/stockanalyzer/filereader/StockFileReader.java
@@ -10,5 +10,11 @@ import java.io.File;
  * Reads stock data from file.
  */
 public interface StockFileReader {
-    StockItem getStockItem(String fileName, Context context);
+    /**
+     * @param pathName path for the file. Often /data/data/<YOUR_APP_PACKAGE_NAME>/files/<FILES_NAME>
+     * @param stockName Name which is assigned to the StockItem which is returned
+     * @param context
+     * @return
+     */
+    StockItem getStockItem(String pathName, String stockName, Context context);
 }
