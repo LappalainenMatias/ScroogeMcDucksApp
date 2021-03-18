3a546ea824b6c5d78ad2e3e7624b7a63dd9a440a
Matias Lappalainen <Matias.lappalainen1@gmail.com>
2021-03-19 00:15:53 +0200

    Refactor and fix error when screen orientation change

--- app/src/main/java/com/example/stockanalyzer/ui/StockListFragment.java
+++ app/src/main/java/com/example/stockanalyzer/ui/StockListFragment.java
@@ -12,6 +12,7 @@ import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Color;
+import android.graphics.drawable.Drawable;
 import android.net.Uri;
 import android.os.Bundle;
 
@@ -25,6 +26,8 @@ import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.WindowManager;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
@@ -52,12 +55,22 @@ public class StockListFragment extends Fragment {
     private TextInputLayout inputStockName;
     private ListView LVStocks;
     private MaterialAlertDialogBuilder materialAlertDialogBuilder;
-    private AlertDialog AlertDialogStockData;
+    private AlertDialog alertDialog;
+    private SearchView searchView;
+    private TextView searchViewInput;
 
     private StockListViewModel stockListViewModel;
     private StockArrayAdapter stockAdapter;
-    private ActivityResultLauncher<Intent> resultLauncher;
-    private Intent intent;
+    private ActivityResultLauncher<Intent> getContentLauncher;
+    private Intent intentGetContent;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getActivity() != null) {
+            stockListViewModel = new ViewModelProvider(this).get(StockListViewModel.class);
+        }
+    }
 
     @Nullable
     @Override
@@ -66,33 +79,33 @@ public class StockListFragment extends Fragment {
         viewContainer = inflater.inflate(R.layout.stocklist_fragment, container, false);
 
         // TODO: 10.3.2021 Refactor. Rename views
-        SearchView searchView = viewContainer.findViewById(R.id.action_search);
+        searchView = viewContainer.findViewById(R.id.action_search);
         LVStocks = viewContainer.findViewById(R.id.LVStocks);
         FloatingActionButton FABAdd = viewContainer.findViewById(R.id.FABAdd);
-        TextView textView = searchView.findViewById(androidx.appcompat.R.id.search_src_text);
-        ImageView searchIcon = searchView.findViewById(androidx.appcompat.R.id.search_button);
-        ImageView searchClose = searchView.findViewById(androidx.appcompat.R.id.search_close_btn);
+        searchViewInput = searchView.findViewById(androidx.appcompat.R.id.search_src_text);
+        ImageView searchViewIcon = searchView.findViewById(androidx.appcompat.R.id.search_button);
+        ImageView searchViewClose = searchView.findViewById(androidx.appcompat.R.id.search_close_btn);
 
-        searchIcon.setImageDrawable(ContextCompat.getDrawable(getActivity(), R.drawable.ic_search_24px));
-        textView.setTextColor(Color.WHITE);
-        textView.setHintTextColor(Color.WHITE);
-        searchClose.setImageDrawable(ContextCompat.getDrawable(getActivity(), R.drawable.ic_clear_24px));
+        searchViewIcon.setImageDrawable(
+                ContextCompat.getDrawable(getActivity(), R.drawable.ic_search_24px));
+        searchViewClose.setImageDrawable(
+                ContextCompat.getDrawable(getActivity(), R.drawable.ic_clear_24px));
+        searchViewInput.setTextColor(Color.WHITE);
+        searchViewInput.setHintTextColor(Color.WHITE);
 
         FABAdd.setOnClickListener(v -> {
-            materialAlertDialogBuilder = new MaterialAlertDialogBuilder(getContext())
-                    .setTitle("Choose a file (.csv) which contains stock data")
-                    .setView(getAttachFileLayout())
-                    .setPositiveButton("ADD", (dialogInterface, i) -> {
-                        if (stockListViewModel.saveStockDataFile(inputStockName.getEditText().getText().toString())) {
-                            Toast.makeText(getContext(), "Successfully saved stock data!", Toast.LENGTH_LONG).show();
-                        } else {
-                            Toast.makeText(getContext(), "Failed to save stock data!", Toast.LENGTH_LONG).show();
-                        }
-                    })
-                    .setNegativeButton("CANCEL", (dialogInterface, i) -> {
-                    });
-            AlertDialogStockData = materialAlertDialogBuilder.show();
-            BTNAddFile = AlertDialogStockData.getButton(DialogInterface.BUTTON_POSITIVE);
+            materialAlertDialogBuilder = createAlertDialogBuilder();
+            alertDialog = materialAlertDialogBuilder.create();
+            alertDialog = materialAlertDialogBuilder.show();
+
+            //Hides keyboard
+            alertDialog.setOnDismissListener(dialogInterface ->
+                    alertDialog.getWindow().setSoftInputMode(
+                            WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN));
+
+            BTNAddFile = alertDialog.getButton(DialogInterface.BUTTON_POSITIVE);
+            BTNAddFile.setAlpha(0.5f);
+            BTNAddFile.setClickable(false);
             stockListViewModel.getSelectedFileUri().setValue(null);
         });
 
@@ -103,8 +116,10 @@ public class StockListFragment extends Fragment {
             }
 
             @Override
-            public boolean onQueryTextChange(String newText) {
-                stockAdapter.getFilter().filter(newText);
+            public boolean onQueryTextChange(String text) {
+                if (stockAdapter != null) {
+                    stockAdapter.getFilter().filter(text);
+                }
                 return false;
             }
         });
@@ -116,50 +131,41 @@ public class StockListFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        stockListViewModel = new ViewModelProvider(this).get(StockListViewModel.class);
-        intent = new Intent(Intent.ACTION_GET_CONTENT);
-        intent.setDataAndType(Uri.parse("/downloads"), "text/csv");
-        resultLauncher = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(),
+        intentGetContent = new Intent(Intent.ACTION_GET_CONTENT);
+        intentGetContent.setDataAndType(Uri.parse("/downloads"), "text/csv");
+
+        getContentLauncher = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(),
                 result -> {
                     if (result.getResultCode() == Activity.RESULT_OK) {
-                        Toast.makeText(getContext(), "File selected!", Toast.LENGTH_SHORT).show();
+                        Toast.makeText(getContext(), "File selected!", Toast.LENGTH_LONG).show();
                         Intent data = result.getData();
-                        Uri uri = data.getData();
-                        stockListViewModel.getSelectedFileUri().setValue(uri);
+                        Uri selectedFileUri = data.getData();
+                        stockListViewModel.getSelectedFileUri().setValue(selectedFileUri);
                     } else {
-                        Toast.makeText(getContext(), "Something went wrong!", Toast.LENGTH_SHORT).show();
+                        Toast.makeText(getContext(), "File was not selected!", Toast.LENGTH_LONG).show();
                     }
                 });
+
         stockListViewModel.getStockItems().observe(getViewLifecycleOwner(), stockItems -> {
             stockAdapter = new StockArrayAdapter(getContext(), getActivity(), stockItems);
             LVStocks.setAdapter(stockAdapter);
+            if(searchViewInput != null){
+                stockAdapter.getFilter().filter(searchViewInput.getText().toString());
+            }
         });
 
         stockListViewModel.getSelectedFileUri().observe(getViewLifecycleOwner(), selectedFileUri -> {
-            // TODO: 12.3.2021 Refactor
-            if (BTNChooseFile == null || inputStockName == null || BTNAddFile == null) {
-                return;
-            }
-            if (selectedFileUri == null) {
-                BTNChooseFile.setVisibility(View.VISIBLE);
-                inputStockName.setVisibility(View.GONE);
-                BTNAddFile.setAlpha(0.5f);
-                BTNAddFile.setClickable(false);
-            } else {
-                AlertDialogStockData.setTitle("Add a name for the stock data");
+            if (selectedFileUri != null && alertDialog != null) {
+                alertDialog.setTitle("Add a name for the stock data");
                 BTNChooseFile.setVisibility(View.GONE);
                 inputStockName.setVisibility(View.VISIBLE);
-                inputStockName.requestFocus();
-
                 inputStockName.getEditText().addTextChangedListener(new TextWatcher() {
                     @Override
                     public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-
                     }
 
                     @Override
                     public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-
                     }
 
                     @Override
@@ -173,6 +179,11 @@ public class StockListFragment extends Fragment {
                         }
                     }
                 });
+
+                //Opens keyboard
+                alertDialog.getWindow().setSoftInputMode(
+                        WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_VISIBLE);
+                inputStockName.getEditText().requestFocus();
             }
         });
 
@@ -183,13 +194,30 @@ public class StockListFragment extends Fragment {
         });
     }
 
+
+    private MaterialAlertDialogBuilder createAlertDialogBuilder() {
+        return new MaterialAlertDialogBuilder(getContext())
+                .setTitle("Choose a file (.csv) which contains stock data")
+                .setView(getAttachFileLayout())
+                .setPositiveButton("ADD", (dialogInterface, i) -> {
+                    String fileName = inputStockName.getEditText().getText().toString();
+                    boolean isFileSaved = stockListViewModel.saveStockDataFile(fileName);
+                    if (isFileSaved) {
+                        Toast.makeText(getContext(), "Successfully saved stock data!", Toast.LENGTH_LONG).show();
+                    } else {
+                        Toast.makeText(getContext(), "Failed to save stock data!", Toast.LENGTH_LONG).show();
+                    }
+                }).setNegativeButton("CANCEL", (dialogInterface, i) -> {
+                });
+    }
+
     private LinearLayout getAttachFileLayout() {
         LayoutInflater inflater = (LayoutInflater) getActivity().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-        LinearLayout linearLayout = (LinearLayout) inflater.inflate(R.layout.choose_file_btn, null, true);
+        LinearLayout linearLayout = (LinearLayout) inflater.inflate(R.layout.choose_file, null, true);
         BTNChooseFile = linearLayout.findViewById(R.id.BTNChooseFile);
         inputStockName = linearLayout.findViewById(R.id.LayoutStocksName);
         BTNChooseFile.setOnClickListener(v -> {
-            resultLauncher.launch(intent);
+            getContentLauncher.launch(intentGetContent);
         });
         return linearLayout;
     }
